ba8517075701bf9c4aeb70f524e133b84b21e12e
unknown <siddharthsalvi99@gmail.com>
2020-12-24 12:32:15 +0530

    Added text field

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import TextField from '@material-ui/core/TextField';
 function App() {
   return (
     <div className="App">
      <h1>This is the deployed app by Siddharth.</h1>
+     <TextField
+                  required
+                  id="outlined-required"
+                  label="First Name"
+                  
+                  variant="outlined"
+                  
+                />
     </div>
   );
 }
